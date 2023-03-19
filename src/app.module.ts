6fef7e82f40447aea24c52085ff2feee1a08f938
Carlos Sullaez <jeraldy.sullaez@agetic.gob.bo>
2023-03-19 03:19:54 -0400

    feat: :rocket: Condiguraciones de base de datos, pretier e instalacion de paquetes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'centro_salud_db',
+        entities: [__dirname + '../../../../**/*.entity{.ts,.js}'],
+        keepConnectionAlive: true,
+        synchronize: false,
+        // logger: new PrintSQL(
+        // configService.get('FORCE_SQL_LOG') === 'true' ||
+        // (configService.get('NODE_ENV') === 'development' &&
+        // configService.get('LOG_SQL') === 'true'),
+        // ),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
