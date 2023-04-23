b1f12d73026c7ddc80272c75706dc6927b297d3d
Carlos Sullaez <jeraldy.sullaez@agetic.gob.bo>
2023-04-23 10:48:12 -0400

    feat: :rocket: Adicion de autenticacion JWT en servicio

--- src/autenticacion/autenticacion.module.ts
+++ src/autenticacion/autenticacion.module.ts
@@ -4,22 +4,29 @@ import { JwtModule } from '@nestjs/jwt'
 import { UsuarioService } from 'src/usuario/service/usuario.service'
 import { AutenticacionController } from './autenticacion.controller'
 import { AutenticacionService } from './autenticacion.service'
-import * as dotenv from 'dotenv'
 import { UsuarioRepository } from 'src/usuario/repository/usuario.repository'
+import { PassportModule } from '@nestjs/passport'
 
+import * as dotenv from 'dotenv'
+import { JwtStrategy } from './strategies/jwt.strategy'
 dotenv.config()
 @Module({
   imports: [
     ConfigModule,
-    // PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       global: true,
-      secret: 'mysecretkey',
-      // secret: configService.get('JWT_SECRET'),
-      signOptions: { expiresIn: '60s' },
+      secret: 'my-secret-key',
+      // secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AutenticacionService, UsuarioService, UsuarioRepository],
+  providers: [
+    AutenticacionService,
+    UsuarioService,
+    UsuarioRepository,
+    JwtStrategy,
+  ],
   controllers: [AutenticacionController],
   exports: [AutenticacionService],
 })
--- src/usuario/controller/usuario.controller.ts
+++ src/usuario/controller/usuario.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 import { UsuarioService } from '../service/usuario.service'
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @Get(':id')
-  async buscarUsuarioPorId(@Param() param) {
-    const { id } = param
+  async buscarUsuarioPorId(@Param('id') id: number): Promise<any> {
     const resultado = this.usuarioService.buscarUsuarioPorId(id)
     return resultado
   }
